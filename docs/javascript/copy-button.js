2d83dc04fec597beabadd755b4b11e537df91ec7
hostedposted <hostedpostedsite@gmail.com>
2022-06-04 11:41:33 -0400

    Revert "Fix copy button"
    This reverts commit 4a21052ef311fe46ca75778bcccd0a361e90d63a.

--- docs/javascript/copy-button.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("copy-button").addEventListener("click", async () => {
-    const text = await (await fetch("https://raw.githubusercontent.com/ProdigyAPI/ProdigyX/master/bookmarklet.txt")).text()
-    navigator.clipboard.writeText(text);
-})
