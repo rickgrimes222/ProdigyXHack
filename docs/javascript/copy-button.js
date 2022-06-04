8f7c1c1c47551049def3432b3e4e6cff877edc70
hostedposted <hostedpostedsite@gmail.com>
2022-06-04 16:40:38 -0400

    Cleaner

--- docs/javascript/copy-button.js
+++ docs/javascript/copy-button.js
@@ -1,4 +1,16 @@
 document.getElementById("copy-button").addEventListener("click", async () => {
     const text = await (await fetch("https://raw.githubusercontent.com/ProdigyAPI/ProdigyX/master/bookmarklet.txt")).text()
-    navigator.clipboard.writeText(text);
+    navigator.clipboard.writeText(text).then(() => {
+        const oldText = document.getElementById("copy-button").innerText
+        document.getElementById("copy-button").innerText = "Copied!"
+        setTimeout(() => {
+            document.getElementById("copy-button").innerText = oldText
+        }, 1000)
+    }).catch(() => {
+        const oldText = document.getElementById("copy-button").innerText
+        document.getElementById("copy-button").innerText = "Failed!"
+        setTimeout(() => {
+            document.getElementById("copy-button").innerText = oldText
+        }, 1000)
+    })
 })
