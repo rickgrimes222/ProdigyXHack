32c4e5ffd20a5f562a7c05e2b054d72b076dd538
hostedposted <hostedpostedsite@gmail.com>
2022-06-24 17:35:25 -0400

    End dino dig

--- src/hacks/minigame.tsx
+++ src/hacks/minigame.tsx
@@ -11,4 +11,12 @@ withCategory(Category.MINIGAME, ({ hack, toggle }) => {
         hack._state.states.get("DinoDig")?.timer?.addTime(8.64e9)
         success("Added 100 days to the timer.")
     })
+    hack("End Dino Dig", "End the current time in dino dig.", async (hack) => {
+        if (!hack._state.states.get("DinoDig")?.timer?.addTime) {
+            error("You are not in dino dig.")
+            return
+        }
+        hack._state.states.get("DinoDig")?.timer?.addTime(-9e15) // Subtract 285,388.1278538812694 years.
+        success("The dino dig game has been ended.")
+    })
 })
