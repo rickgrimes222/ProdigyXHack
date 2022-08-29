40aac83343c66f42826ef794f2bb5a9d10e340a4
AGR <63412264+AGR-DEVS@users.noreply.github.com>
2022-08-28 20:21:27 -0400

    ui toggle update

--- src/hacks/base/hack.tsx
+++ src/hacks/base/hack.tsx
@@ -34,7 +34,13 @@ const Hack: FunctionalComponent<HackProps> = ({ name, description, hackFunction 
         }
     }
 
-    return <button onClick={onClick} title={description} className="capitalize odd:bg-blue-600 even:bg-emerald-600 forest:odd:bg-pink-400 forest:even:bg-orange-400 shiverchill:odd:bg-[#5AC6FF] shiverchill:even:bg-[#7589CC] bonfire-spire:odd:bg-[#F76E11] bonfire-spire:even:!text-black bonfire-spire:even:bg-[#FF9F45] mt-2 md:mb-0 mr-3 text-white shadow-xl forest:shadow-md odd:shadow-blue-400 even:shadow-emerald-400 forest:odd:shadow-pink-400 forest:even:shadow-orange-400 shiverchill:odd:shadow-[#5AC6FF] shiverchill:even:shadow-[#7589CC] bonfire-spire:odd:shadow-[#F76E11] bonfire-spire:even:shadow-[#FF9F45] rounded px-4 py-1.5" data-mdb-ripple="true" data-mdb-ripple-color="light">{name}</button>
+    return (
+        <div data-mdb-ripple="true" data-mdb-ripple-color="light" className="capitalize rounded px-4 py-1.5 text-center odd:bg-blue-600 even:bg-emerald-600 forest:odd:bg-pink-400 forest:even:bg-orange-400 shiverchill:odd:bg-[#5AC6FF] shiverchill:even:bg-[#7589CC] bonfire-spire:odd:bg-[#F76E11] bonfire-spire:even:!text-black bonfire-spire:even:bg-[#FF9F45] mt-2 md:mb-0 mr-3 text-white shadow-xl forest:shadow-md odd:shadow-blue-400 even:shadow-emerald-400 forest:odd:shadow-pink-400 forest:even:shadow-orange-400 shiverchill:odd:shadow-[#5AC6FF] shiverchill:even:shadow-[#7589CC] bonfire-spire:odd:shadow-[#F76E11] bonfire-spire:even:shadow-[#FF9F45] ">
+            <button onClick={onClick} title={description}>
+                {name}
+            </button>
+        </div>
+    )
 }
 
 export default Hack
--- src/hacks/base/toggle.tsx
+++ src/hacks/base/toggle.tsx
@@ -40,9 +40,9 @@ const Toggle: FunctionalComponent<ToggleProps> = ({ name, toggleFunction, checke
     }
 
     return (
-        <div className="pl-10 group mt-2">
-            <input className="checked:group-odd:bg-blue-600 checked:group-even:bg-emerald-600 forest:checked:group-odd:bg-pink-400 forest:checked:group-even:bg-orange-400 shiverchill:checked:group-odd:bg-[#5AC6FF] shiverchill:checked:group-even:bg-[#7589CC] bonfire-spire:checked:group-odd:bg-[#F76E11] bonfire-spire:checked:group-even:bg-[#FF9F45] switch-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-no-repeat bg-contain bg-zinc-400 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id={`select-hack-${name}`} checked={toggled} onClick={onChange} />
-            <label className="form-check-label inline-block text-gray-800 mr-3" htmlFor={`select-hack-${name}`}>{name}</label>
+        <div className="rounded px-4 py-1 pl-12 group mt-2 text-center odd:bg-blue-600 even:bg-emerald-600 forest:odd:bg-pink-400 forest:even:bg-orange-400 shiverchill:odd:bg-[#5AC6FF] shiverchill:even:bg-[#7589CC] bonfire-spire:odd:bg-[#F76E11] bonfire-spire:even:!text-black bonfire-spire:even:bg-[#FF9F45] md:mb-0 mr-3 text-white shadow-xl forest:shadow-md odd:shadow-blue-400 even:shadow-emerald-400 forest:odd:shadow-pink-400 forest:even:shadow-orange-400 shiverchill:odd:shadow-[#5AC6FF] shiverchill:even:shadow-[#7589CC] bonfire-spire:odd:shadow-[#F76E11] bonfire-spire:even:shadow-[#FF9F45]">
+            <input className="checked:group-even:bg-blue-600 checked:group-odd:bg-emerald-600 forest:checked:group-even:bg-pink-400 forest:checked:group-odd:bg-orange-400 shiverchill:checked:group-odd:bg-[#7589CC] shiverchill:checked:group-even:bg-[#5AC6FF] bonfire-spire:checked:group-odd:bg-[#FF9F45] bonfire-spire:checked:group-even:bg-[#F76E11] switch-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-no-repeat bg-contain bg-zinc-400 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id={`select-hack-${name}`} checked={toggled} onClick={onChange} />
+            <label className="form-check-label inline-block font-normal mr-3" htmlFor={`select-hack-${name}`}>{name}</label>
         </div>
     )
 }
