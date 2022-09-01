9ff9c7c6ffe08eb27a162edbc6cf9c073358f81b
hostedposted <hostedpostedsite@gmail.com>
2022-09-01 18:05:01 -0400

    Better Gold Limit

--- src/hacks/player.tsx
+++ src/hacks/player.tsx
@@ -7,7 +7,7 @@ import { withCategory } from "./base/registry"
 
 withCategory(Category.PLAYER, ({ hack, toggle }) => {
     hack("Set Gold", "Set's the amount of gold you have currently.", async (hack, player) => {
-        const value = await InputTypes.integer("Please enter the amount of gold you want to get.", 1, 9999999)
+        const value = await InputTypes.integer("Please enter the amount of gold you want to get.", 1, 9000000)
         player.data.gold = value
         success(`You now have ${value} gold.`)
     })
