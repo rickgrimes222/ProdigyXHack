37a8810b7f369df711dc5bed2c66f33a69f1a790
hostedposted <hostedpostedsite@gmail.com>
2022-09-10 14:28:14 -0400

    Names

--- src/components/ChatMenu.tsx
+++ src/components/ChatMenu.tsx
@@ -3,13 +3,23 @@ import { GoPrimitiveDot } from "react-icons/go"
 import { useEffect, useState, useRef } from "preact/hooks"
 import { io, Socket } from "socket.io-client"
 import MenuToggler from "./MenuToggler"
+import { getPlayer } from "../hack"
+import { Player } from "../types/player"
+
+interface Message {
+    message: string
+    name: string
+}
 
 const ChatMenu: FunctionalComponent = () => {
     const [visible, setVisible] = useState<boolean>(false)
     const [userCount, setUserCount] = useState<number>(0)
     const [socket, setSocket] = useState<Socket>(null)
-    const [messages, setMessages] = useState<string[]>([])
+    const [messages, setMessages] = useState<Message[]>([])
     const inputRef = useRef<HTMLInputElement>(null)
+    const messageRef = useRef<HTMLDivElement>(null)
+
+    const player = getPlayer() as Player
 
     useEffect(() => {
         const socket = io("https://prodigy-x-chat.herokuapp.com")
@@ -17,31 +27,40 @@ const ChatMenu: FunctionalComponent = () => {
         socket.on("userCount", (count: number) => {
             setUserCount(count)
         })
-        socket.on("chat", (message: string) => {
+        socket.on("chat", (message: Message) => {
             setMessages(messages => [...messages, message])
         })
     }, [])
+
+    useEffect(() => {
+        if (messageRef.current) {
+            messageRef.current.scrollTop = messageRef.current.scrollHeight
+        }
+    }, [messages])
 
     function onSubmit (e: Event) {
         e.preventDefault()
         const input = inputRef.current
         if (input.value) {
-            socket.emit("chat", input.value)
+            socket.emit("chat", {
+                message: input.value,
+                name: player.getName()
+            })
             input.value = ""
         }
     }
 
     return (
         <div>
-            <div className="absolute rounded w-1/5 h-2/5 bottom-8 right-8 bg-gray-200 bg-opacity-90" id="chat-mainframe" data-visible={visible}>
+            <div className="absolute rounded w-1/5 h-2/5 bottom-16 right-8 bg-gray-200 bg-opacity-90" id="chat-mainframe" data-visible={visible}>
                 <div>
                     <p className="w-1/2 pl-3 text-lg font-bold inline-block">Prodigy X Chat</p>
                     {/* @ts-ignore */}
                     <p className="w-1/2 pr-3 text-right text-sm font-bold inline-block text-[#5fc4b9]"><GoPrimitiveDot className="inline-block" color="#5fc4b9" />{userCount} Online</p>
                 </div>
-                <div className="flex flex-col-reverse rounded overflow-y-scroll m-6 p-2 bg-gray-300 bg-opacity-90 w-2/2 h-3/4">
+                <div className="flex flex-col rounded overflow-y-scroll m-6 p-2 bg-gray-300 bg-opacity-90 w-2/2 h-3/4" ref={messageRef}>
                     {messages.map((message, index) => {
-                        return <div className="shrink-0 grow-0" key={index}>Player Name: {message}</div>
+                        return <div className="shrink-0 grow-0" key={index}>{message.name}: {message.message}</div>
                     })}
                 </div>
                 <form onSubmit={onSubmit}>
