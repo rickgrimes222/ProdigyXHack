9672fc72ff84fef983f5f3fefab3d144e9cc8b80
AGR <63412264+AGR-DEVS@users.noreply.github.com>
2022-09-20 16:23:57 -0400

    badges

--- src/components/ChatMenu.tsx
+++ src/components/ChatMenu.tsx
@@ -10,6 +10,7 @@ interface Message {
     message: string
     name: string
     id: number
+    badge?: string
 }
 
 const ChatMenu: FunctionalComponent = () => {
@@ -66,6 +67,9 @@ const ChatMenu: FunctionalComponent = () => {
                             <div className="rounded bg-gray-300 m-1 p-2" key={index}>
                                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/2048px-Default_pfp.svg.png" alt={`${message.name}'s Avatar`} role="button" className="w-10 h-10 float-left mr-3" onClick={() => launchCard(message.id)} />
                                 <button className="font-bold text-sm" onClick={() => launchCard(message.id)}>{message.name}</button>
+                                <span className={`text-xs font-semibold ${message.badge ? "visible" : "invisible"} inline py-1 px-2 rounded-full text-blue-600 bg-blue-200 mx-2`}>
+                                    {message.badge}
+                                </span>
                                 <p className="text-sm">{message.message}</p>
                             </div>
                         )
